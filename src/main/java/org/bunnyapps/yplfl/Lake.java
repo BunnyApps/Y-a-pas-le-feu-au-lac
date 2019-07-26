a5d741504d805dca172d606cb3912197b9e2963f
BunnyApps <teambunnyapps@gmail.com>
2019-07-26 19:44:23 +0200

    Started adding France lakes.

--- src/main/java/org/bunnyapps/yplfl/Lake.java
+++ src/main/java/org/bunnyapps/yplfl/Lake.java
@@ -18,8 +18,134 @@ package org.bunnyapps.yplfl;
 
 public enum Lake {
 
+	////////////
+	// France //
+	////////////
+
+	// Haute-Garonne
+
+	/** Espingo lake. */
+	ESPINGO(Country.FRANCE, "lac d'Espingo", 42.728611, 0.495556, 1882.0, Double.NaN, false, true),
+
+	/** Laragou. */
+	LARAGOU(Country.FRANCE, "lac du Laragou", 43.694167, 1.658889, 173.0, 470000.0, true, true),
+
+	/** Oô lake. */
+	OO(Country.FRANCE, "lac d'Oô", 42.74, 0.491944, 1507.0, Double.NaN, false, true),
+
+	/** Portillon lake. */
+	PORILLON(Country.FRANCE, "lac du Portillon", 42.700833, 0.508889, 2580.0, Double.NaN, false, true),
+
+	/** Saint-Ferreol lake. */
+	SAINT_FERREOL(Country.FRANCE, "lac de Saint-Ferréol", 43.4375, 2.021667, 150.0, 670000.0, true, true),
+
+	/** Gimone lake. */
+	GIMONE(Country.FRANCE, "lac de la Gimone", 43.329722, 0.671389, 332.0, 2500000, true, true),
+
+	/** Flourens lake. */
+	FLOURENS(Country.FRANCE, "lac de Flourens", 43.596642, 1.564871, 152.0, 75000.0, true, true),
+
+	/** Sainte Foy d'Aigrefeuille lake. */
+	FOY_AIGREFEUILLE(Country.FRANCE, "lac de Sainte Foy d'Aigrefeuille", 43.548028, 1.607808, 152.0, 50000.0, true, true),
+
+	/** Labege-Innopole lake. */
+	LABEGE_INNOPOLE(Country.FRANCE, "lac de Labège Innopole", 43.538347, 1.514166, 152.0, 60000.0, true, true),
+
+	/** Rabaudy lake. */
+	RABAUDY(Country.FRANCE, "lac de Rabaudy", 43.51677, 1.512426, 152.0, 20000.0, true, true),
+
+	/** Sesquieres lake. */
+	SESQUIERES(Country.FRANCE, "lac de Sesquieres", 43.653249, 1.418836, 152.0, 115000.0, true, true),
+
+	/** Raby lake. */
+	RABY(Country.FRANCE, "lac de Raby", 43.714774, 1.368589, 152.0, 100000.0, true, true),
+
+	/** Ritouret lake. */
+	RITOURET(Country.FRANCE, "lac de Ritouret", 43.634240, 1.385318, 152.0, 15000.0, true, true),
+
+	/** Gironis lake. */
+	GIRONIS(Country.FRANCE, "lac de Gironis", 43.564294, 1.416890, 152.0, 30000.0, true, true),
+
+	/** Bois Vieux lake. */
+	BOIS_VIEUX(Country.FRANCE, "lac du Bois Vieux", 43.519701, 1.354264, 152.0, 20000.0, true, true),
+
+	/** General lake. */
+	GENERAL(Country.FRANCE, "lac de la General", 43.522837, 1.421493, 152.0, 80000.0, true, true),
+
+	/** Lamartine lake. */
+	LAMARTINE(Country.FRANCE, "lac de Lamartine", 43.505788, 1.341487, 152.0, 150000.0, true, true),
+
+	/** Arc-En-Ciel lake. */
+	ARCENCIEL(Country.FRANCE, "lac Arc en Ciel", 43.587887, 1.362267, 152.0, 15000.0, false, true),
+
+	/** Vieux pigeonnier lake. */
+	VIEUX_PIGEONNIER(Country.FRANCE, "lac du Vieux Pigeonnier", 43.585105, 1.358822, 152.0, 20000.0, true, true),
+
+	/** Tournefeuille lake. */
+	TOURNEFEUILLE(Country.FRANCE, "lac de Tournefeuille", 43.579656, 1.349433, 152.0, 20000.0, true, true),
+
+	/** Ramee lake. */
+	RAMEE(Country.FRANCE, "lac de la Ramee", 43.569247, 1.349913, 152.0, 440000.0, true, true),
+
+	/** Loustalet lake. */
+	LOUSTALET(Country.FRANCE, "lac de Loustalet", 43.575280, 1.309080, 152.0, 20000.0, true, true),
+
+	/** Soula lake. */
+	SOULA(Country.FRANCE, "lac de Soula", 43.565082, 1.308831, 152.0, 40000.0, true, true),
+
+	/** Launac lake. */
+	LAUNAC(Country.FRANCE, "lac de Launac", 43.744992, 1.176844, 152.0, 100000.0, true, true),
+
+	/** Four de Louge lake. */
+	FOUR_LOUGE(Country.FRANCE, "lac Four de Louge", 43.441810, 1.305909, 152.0, 125000.0, true, true),
+
+	/** Lavernose lake. */
+	LAVERNOSE(Country.FRANCE, "lac de Lavernose", 43.387947, 1.265633, 152.0, 180000.0, true, true),
+
+	/** Peyssies lake. */
+	PAYSSIES(Country.FRANCE, "lac de Peyssies", 43.328758, 1.171653, 152.0, 170000.0, true, true),
+
+	/** Barbis lake. */
+	BARBIS(Country.FRANCE, "lac de Barbis", 43.322001, 1.201197, 152.0, 80000.0, true, true),
+
+	/** L'Orme Blanc lake. */
+	ORME_BLANC(Country.FRANCE, "lac de L Orme Blanc", 43.528062, 1.745819, 152.0, 50000.0, true, true),
+
+	/** Galdou lake. */
+	GALDOU(Country.FRANCE, "lac de Galdou", 43.494393, 1.763916, 152.0, 110000.0, true, true),
+
+	/** Cambiac lake. */
+	CAMBIAC(Country.FRANCE, "lac de Cambiac", 43.499356, 1.791093, 152.0, 110000.0, true, true),
+
+	/** Vaux lake. */
+	VAUX(Country.FRANCE, "lac de Vaux", 43.452702, 1.838243, 152.0, 150000.0, true, true),
+
+	/** Enclas lake. */
+	ENCLAS(Country.FRANCE, "lac de Enclas", 43.427580, 1.896554, 152.0, 30000.0, true, true),
+
+	/** Vallegue lake. */
+	VALLEGUE(Country.FRANCE, "lac de Vallegue", 43.421648, 1.752705, 152.0, 80000.0, true, true),
+
+	/** Condoumine lake. */
+	COUDOUMINE(Country.FRANCE, "lac de Condoumine", 43.421648, 1.752705, 152.0, 100000.0, true, true),
+
+	/** Thesauque lake. */
+	THESAUQUE(Country.FRANCE, "lac de Thesauque", 43.358246, 1.645760, 152.0, 330000.0, true, true),
+
+	/** Bourg Saint-Bernard lake. */
+	BOURG_SAINT_BERNARD(Country.FRANCE, "lac de Bourg Saint Bernard", 43.586074, 1.703336, 152.0, 270000.0, true, true),
+
+	/** Balerme lake. */
+	BALERNE(Country.FRANCE, "le Balerme", 43.651555, 1.702139, 152.0, 390000.0, true, true),
+
+	/** Saint Andre lake. */
+	SAINT_ANDRE(Country.FRANCE, "lac Saint Andre", 43.300957, 0.862649, 152.0, 450000.0, true, true),
+
+	/** Saint Lys lake. */
+	SAINT_LYS(Country.FRANCE, "lac Saint Lys", 43.517967, 1.192156, 152.0, 20000.0, true, true),
+
 	/** Unknown lake. */
-	UNKNOWN(Country.UNKNOWN, "unknown", Double.NaN, Double.NaN, Double.NaN, false, false);
+	UNKNOWN(Country.UNKNOWN, "unknown", Double.NaN, Double.NaN, Double.NaN, Double.NaN, false, false);
 
 	/** Country where the lake is setting. */
 	private final Country country;
@@ -49,13 +175,14 @@ public enum Lake {
 	 * @param latitude lake geodetic latitude in degrees
 	 * @param longitude lake geodetic longitude in degrees
 	 * @param altitude lake altitude in meters
+	 * @param area area of the lake in m²
 	 * @param isParking true if a parking is available
 	 * @param isFree true is the lake is free access
 	 */
 	Lake(final Country country, final String name,
 		 final double latitude, final double longitude,
-		 final double altitude, final boolean isParking,
-		 final boolean isFree) {
+		 final double altitude, double area,
+		 final boolean isParking, final boolean isFree) {
 		this.country   = country;
 		this.name      = name;
 		this.latitude  = latitude;
